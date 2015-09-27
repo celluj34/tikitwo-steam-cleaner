9f257b41eab2510c457206881f7d9cfd11b83f75
Joe Cellucci <celluj34@gmail.com>
2015-09-27 17:08:49 -0500

    get two-way bindings working

--- tikitwo-steam-cleaner/ViewModels/MainViewModel.cs
+++ tikitwo-steam-cleaner/ViewModels/MainViewModel.cs
@@ -2,5 +2,20 @@
 
 namespace tikitwo_steam_cleaner.UAP.ViewModels
 {
-    public class MainViewModel : BindableBase {}
+    public class MainViewModel : BindableBase
+    {
+
+        private string _steamFolder;
+        public string SteamFolder
+        {
+            get {return _steamFolder;}
+            set
+            {
+                if(SetProperty(ref _steamFolder, value))
+                {
+                    //alert or something
+                }
+            }
+        }
+    }
 }
