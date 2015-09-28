ccf3460ccf07b01ab153b11579446df201fbe3c6
Joe Cellucci <celluj34@gmail.com>
2015-09-27 21:31:15 -0500

    add a few more commands and a list prop for the folders

--- tikitwo-steam-cleaner.WPF/ViewModels/MainViewModel.cs
+++ tikitwo-steam-cleaner.WPF/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Prism.Commands;
 using Prism.Mvvm;
 using tikitwo_steam_cleaner.Application.Services;
 
@@ -15,12 +17,17 @@ namespace tikitwo_steam_cleaner.WPF.ViewModels
             _steamFolderService = steamFolderService;
 
             FindSteamFolder = new DelegateCommand(FindSteamFolderExecute, CanFindSteamFolder);
+            AddFolder = new DelegateCommand(AddFolderExecute, CanAddFolder);
+            RemoveCustomFolder = new DelegateCommand(RemoveCustomFolderExecute, CanRemoveCustomFolder);
+            Search = new DelegateCommand(SearchExecute, CanSearch);
 
             CanUseControls = true;
+            FoldersToSearch = new ObservableCollection<string>();
         }
 
         #region Private Backing Fields
         private bool _canUseControls;
+        private string _selectedFolder;
         #endregion
 
         #region Public Properties
@@ -36,6 +43,42 @@ namespace tikitwo_steam_cleaner.WPF.ViewModels
             }
         }
 
+        public ObservableCollection<string> FoldersToSearch {get;set;}
+
+        public string SelectedFolder
+        {
+            get {return _selectedFolder;}
+            set
+            {
+                if(SetProperty(ref _selectedFolder, value))
+                {
+                    UpdateCommands();
+                }
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        private void UpdateCommands()
+        {
+            FindSteamFolder.RaiseCanExecuteChanged();
+            AddFolder.RaiseCanExecuteChanged();
+            RemoveCustomFolder.RaiseCanExecuteChanged();
+            Search.RaiseCanExecuteChanged();
+        }
+
+        private void AddFolderToDisplay(string newFolder)
+        {
+            if(newFolder == null || FoldersToSearch.Contains(newFolder))
+            {
+                return;
+            }
+
+            FoldersToSearch.Add(newFolder);
+
+            UpdateCommands();
+        }
+        #endregion
 
         #region Commands
 
@@ -55,17 +98,48 @@ namespace tikitwo_steam_cleaner.WPF.ViewModels
         }
         #endregion
 
+        #region Add Folder
+        public DelegateCommand AddFolder {get;}
 
+        private void AddFolderExecute()
         {
+            var newFolder = _steamFolderService.GetFolder();
+
+            AddFolderToDisplay(newFolder);
+        }
+
+        private bool CanAddFolder()
+        {
+            return CanUseControls;
+        }
+        #endregion
+
+        #region Remove Custom Folder
+        public DelegateCommand RemoveCustomFolder {get;}
+
+        private void RemoveCustomFolderExecute()
+        {
+            FoldersToSearch.Remove(SelectedFolder);
+        }
+
+        private bool CanRemoveCustomFolder()
+        {
+            return CanUseControls && FoldersToSearch.Any() && SelectedFolder != null;
         }
         #endregion
 
         #region Search
+        public DelegateCommand Search {get;}
 
-        //private bool CanSearch()
-        //{
-        //    return !string.IsNullOrWhiteSpace(SteamFolder) && Directory.Exists(SteamFolder);
-        //}
+        private void SearchExecute()
+        {
+            CanUseControls = false;
+        }
+
+        private bool CanSearch()
+        {
+            return CanUseControls && FoldersToSearch.Any();
+        }
         #endregion
 
         #endregion
