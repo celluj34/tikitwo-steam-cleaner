efa8ed9ed1d01674f37421f8e00be79a83153316
Joe Cellucci <celluj34@gmail.com>
2015-10-11 19:28:51 -0500

    add test project

---
+++ tikitwo-steam-cleaner.Test/CommonSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace tikitwo_steam_cleaner.Test
+{
+    [Binding]
+    public class CommonSteps
+    {
+        [Given(@"I have application settings")]
+        public void GivenIHaveApplicationSettings(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I load the application settings")]
+        public void WhenILoadTheApplicationSettings()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should have application settings")]
+        public void ThenIShouldHaveApplicationSettings(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ tikitwo-steam-cleaner.Test/Services/AppSettings.feature
@@ -0,0 +1,10 @@
+﻿Feature: AppSettings
+
+Scenario: application settings can be mocked as a sanity check
+Given I have application settings
+    | Key  | Value  |
+    | key1 | value1 |
+When I load the application settings
+Then I should have application settings
+    | Key  | Value  |
+    | key1 | value1 |
